3a5c010dc8c08d23ceb4d6836ecfd9c15470c429
TheTrooble <thetrooble@gmail.com>
2017-11-13 22:34:00 -0500

    Greetings from cancer, let's end Germany

---
+++ gdqDonation.go
@@ -0,0 +1,27 @@
+package main
+
+import(
+    "fmt"
+    "log"
+    "github.com/bwmarrin/discordgo"
+    "net/http"
+	"io/ioutil"
+) 
+
+func gdq(s *discordgo.Session, msg *discordgo.MessageCreate, comp string){
+    resp, err := http.Get("http://taskinoz.com/gdq/api/")
+	if err != nil {
+		log.Fatal("GDQ  error:", err)
+	}
+	defer resp.Body.Close()
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatal("GDQ  error:", err)
+	}
+	s.ChannelMessageSend(msg.ChannelID,string(body))
+	fmt.Println(string(body))
+}
+
+func init() {
+	CmdList["donation"] = gdq
+}
