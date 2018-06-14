1a18300fc97bfe78d9a516c2e42b7377bd6357d7
cakebombs <heartasword@gmail.com>
2018-06-13 22:32:19 -0400

    Twitch Preview Fix and new roles
    Fixed twitch preview not updating and added civ and bombs roles

--- groups.go
+++ groups.go
@@ -9,6 +9,7 @@ func joinGroup(s *discordgo.Session, msg *discordgo.MessageCreate, arg string) {
     group, _ := grabArg(arg)
     channel, _ := s.Channel(msg.ChannelID)
     guildID := channel.GuildID
+<<<<<<< Updated upstream
     switch strings.ToLower(group) {
     case "overwatch":
         s.GuildMemberRoleAdd(guildID, msg.Author.ID, "190633106842058754")
@@ -33,6 +34,38 @@ func joinGroup(s *discordgo.Session, msg *discordgo.MessageCreate, arg string) {
         s.ChannelMessageSend(msg.ChannelID, "You have joined Deceit!")
     default:
         s.ChannelMessageSend(msg.ChannelID, "I can add you to the following groups: ```Overwatch\nTabletop\nMinors\nTiles\nCars\nPUBG\nDeceit```")
+=======
+    switch strings.ToLower(group){
+        case "overwatch":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"190633106842058754")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Overwatch!")
+        case "tabletop":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"270691313911857165")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Tabletop Simulator!")
+        case "minors":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"250769687997186048")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined the Minors!")
+        case "tiles":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"276514629700681728")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Meme Tiles!")
+        case "cars":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"277545381993381889")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Rocket Cars!")
+        case "pubg":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"304049346846916608")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined PUBG!")
+        case "deceit":
+            s.GuildMemberRoleAdd(guildID,msg.Author.ID,"376550070029385749")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Deceit!")
+        case "bombs":
+            s.GuildMemberRoleAdd(guildID, msg.Author.ID, "424309460467449864")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Bombs!")
+        case "civ":
+            s.GuildMemberRoleAdd(guildID, msg.Author.ID, "349279584694566934")
+            s.ChannelMessageSend(msg.ChannelID, "You have joined Civ!")
+        default:
+            s.ChannelMessageSend(msg.ChannelID, "I can add you to the following groups: ```Overwatch\nTabletop\nMinors\nTiles\nCars\nPUBG\nDeceit```")
+>>>>>>> Stashed changes
     }
 }
 
@@ -40,6 +73,7 @@ func leaveGroup(s *discordgo.Session, msg *discordgo.MessageCreate, arg string) 
     group, _ := grabArg(arg)
     channel, _ := s.Channel(msg.ChannelID)
     guildID := channel.GuildID
+<<<<<<< Updated upstream
     switch strings.ToLower(group) {
     case "overwatch":
         s.GuildMemberRoleRemove(guildID, msg.Author.ID, "190633106842058754")
@@ -64,6 +98,38 @@ func leaveGroup(s *discordgo.Session, msg *discordgo.MessageCreate, arg string) 
         s.ChannelMessageSend(msg.ChannelID, "You have left Deceit!")
     default:
         s.ChannelMessageSend(msg.ChannelID, "I can remove you from the following groups: ```Overwatch\nTabletop\nMinors\nTiles\nCars\nPUBG\nDeceit```")
+=======
+    switch strings.ToLower(group){
+        case "overwatch":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"190633106842058754")
+            s.ChannelMessageSend(msg.ChannelID, "You have left Overwatch!")
+        case "tabletop":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"270691313911857165")
+            s.ChannelMessageSend(msg.ChannelID, "You have left Tabletop Simulator!")
+        case "minors":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"250769687997186048")
+            s.ChannelMessageSend(msg.ChannelID, "You have left the Minors!")
+        case "tiles":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"276514629700681728")
+            s.ChannelMessageSend(msg.ChannelID, "You have left Meme Tiles!")
+        case "cars":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"277545381993381889")
+            s.ChannelMessageSend(msg.ChannelID, "You have left Rocket Cars!")
+        case "pubg":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"304049346846916608")
+            s.ChannelMessageSend(msg.ChannelID, "You have left PUBG!")
+        case "deceit":
+            s.GuildMemberRoleRemove(guildID,msg.Author.ID,"376550070029385749")
+            s.ChannelMessageSend(msg.ChannelID, "You have left Deceit!")
+	    case "bombs":
+		    s.GuildMemberRoleRemove(guildID, msg.Author.ID, "424309460467449864")
+		    s.ChannelMessageSend(msg.ChannelID, "You have left Bombs!")
+	    case "civ":
+		    s.GuildMemberRoleRemove(guildID, msg.Author.ID, "349279584694566934")
+		    s.ChannelMessageSend(msg.ChannelID, "You have left Civ!")
+        default:
+            s.ChannelMessageSend(msg.ChannelID, "I can remove you from the following groups: ```Overwatch\nTabletop\nMinors\nTiles\nCars\nPUBG\nDeceit```")
+>>>>>>> Stashed changes
     }
 }
 
